30d78bef277d3c61647df76b899f2cc5f1e654ff
Josef Renner <josef.renner@gmail.com>
2013-08-15 00:53:25 +0200

    footer eingemittet und einige textauszeichnungen ergänzt ...

--- footer.php
+++ footer.php
@@ -3,8 +3,7 @@
 			<div class="credit">
 			 	&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> | 
 			 	powered by	<a href="http://wordpress.org/" title="wordpress.org">WordPress</a>
-				with <a href="http://shaheeilyas.com/barecity" title="barecity 1.5 &raquo; shaheeilyas.com">Barecity</a> and 
-				<a href="http://www.deliciousdays.com/cforms-plugin/" title="cforms II &raquo; delicious:days">cforms II</a>.
+				with <a href="http://josefrenner.ch" title="mOMo Theme 1.0 &raquo; josefrenner.ch">mOMoTheme</a>.
 			</div>
 	</div><!-- end rap -->
 </body>
