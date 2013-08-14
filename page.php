68684c9f6e2eedd48f9f5a4bc84892d3446732c8
Josef Renner <josef.renner@gmail.com>
2013-08-14 21:45:34 +0200

    added variables for all colors so far, removed title from content

--- page.php
+++ page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<div class="post single" id="post-<?php the_ID(); ?>">
-		<h3 class="storytitle"><?php the_title(); ?></h3>
+		<!-- <h3 class="storytitle"><?php the_title(); ?></h3> -->
 			<div class="storycontent">
 				<?php the_content('<p class="serif">' . __('(more...)') . '</p>'); ?>
 
